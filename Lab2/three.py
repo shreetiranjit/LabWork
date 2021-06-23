9f6f5839379e39288a96debbcc0ad0ffc16eb532
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-23 14:27:13 +0545

    oddeven

---
+++ Lab2/three.py
@@ -0,0 +1,8 @@
+'''
+ Check whether the user input number is even or odd and display it to user.
+'''
+number = int(input("Enter your number:"))
+if (number%2==0 ) :
+    print (f"{number} is even .")
+else :
+    print(f"{number} is odd .")
