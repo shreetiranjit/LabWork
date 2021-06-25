7a6152d5e958bb722d86d8d3800dd4ae891d705b
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-25 13:39:08 +0545

    upperandlower

---
+++ Lab3/seven.py
@@ -0,0 +1,6 @@
+'''
+Write a Python function that accepts a string and calculate the number of upper case letters and lower
+case letters
+'''
+def upperandlower() :
+    up = str(input("Enter the sentence:"))
