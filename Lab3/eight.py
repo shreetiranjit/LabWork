73548401c5bf1aaa8390285732dd0531ea06c0dc
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-25 14:51:26 +0545

    prime number

--- Lab3/eight.py
+++ Lab3/eight.py
@@ -3,18 +3,18 @@ Write a Python function that takes a number as a parameter and check the number 
 Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors
 other than 1 and itself.
 '''
-def prime(a) :
+def prime(p) :
 
-    if a > 1 :
-        for i in range (2,a) :
-           if a % i == 0 :
-               print (f'{a} is not a prime number.')
+    if p > 1 :
+        for i in range (2,p) :
+           if p % i == 0 :
+               print (f'{p} is not a prime number.')
                break
            else :
-               print (f'{a} is a prime number.')
+               print (f'{p} is a prime number.')
                break
     else :
-        print(f'{a} is not a prime number.')
-    return a
-a = int (input("Enter the number:"))
-prime(a)
+        print(f'{p} is not a prime number.')
+    return p
+p = int (input("Enter the number:"))
+prime(p)
