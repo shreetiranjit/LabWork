f9bfd246590375d2de7ca40bcc735cd5b97646ba
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-25 15:14:12 +0545

    factorial

--- Lab3/eleven.py
+++ Lab3/eleven.py
@@ -2,11 +2,13 @@
 Write a program to find the factorial of a number using functions.
 '''
 def factorial(num):
-    '''
-    factorial of number
-    :param num: float , integer
-    :return: float , integer
-    '''
-    for i in range (2,num+1) :
-        product = product * i
-        return product
+
+    fact = 1
+    for i in range (2, num + 1) :
+        fact = fact * i
+
+    return fact
+num = int(input("Enter the value:"))
+print (factorial(num))
+
+
