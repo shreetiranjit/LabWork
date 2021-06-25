8d698414324b02f52f38b531bc35babafbd05993
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-25 13:12:50 +0545

    stars

---
+++ Lab3/five.py
@@ -0,0 +1,14 @@
+'''
+ Write a function called show_stars(rows).If rows is 5, it should print the following:
+ *
+ **
+ ***
+ ****
+ *****
+'''
+def show_stars(a):
+    for i in range (a) :
+        for j in range (i+1) :
+           print ('*' , end = '')
+        print()
+show_stars(5)
