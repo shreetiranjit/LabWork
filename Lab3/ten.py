5daf7aa6ef2f887bf96c6e14377ca63fe75fe261
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-25 14:56:50 +0545

    evenindex

---
+++ Lab3/ten.py
@@ -0,0 +1,14 @@
+'''
+Accept string from the user and display only those characters which are present at an even index
+'''
+
+def string(a):
+   evenstring = a[::2]
+   print (f'{evenstring} are the characters in even index.')
+   return a
+a = str(input("Enter the word:"))
+string(a)
+
+
+
+
