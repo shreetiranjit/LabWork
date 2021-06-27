61d725237c318b7ce71871d0159a87171c04301e
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-27 12:22:26 +0545

    limit

--- Lab3/four.py
+++ Lab3/four.py
@@ -2,17 +2,11 @@
  Write a function that returns the sum of multiples of 3 and 5 between 0 and limit(parameter). For
  example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20
 '''
-def multiples():
-    i = 1
-    a = int(input("Enter the value :"))
-    for i in range (0, 11) :
-        c = a * i
-        if (c <=20) :
-            print (f' {a} * {i} is {c}' )
-
-    i = i + 1
-    d = c
-    sum = d+ c
-
-multiples()
-multiples()
+def sumofnumbers(limit):
+    sum = 0
+    for i in range(1,limit):
+        if i%3==0 or i%5==0:
+            sum+=i
+    i+=1
+    return sum
+print(sumofnumbers(11))
