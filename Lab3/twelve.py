f9fbbe761dd0e9e4fdd26a168aa0de75b0f3836e
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-28 13:30:54 +0545

    armstrong

--- Lab3/twelve.py
+++ Lab3/twelve.py
@@ -3,18 +3,19 @@ Write a python program to check whether the number is Armstrong number or not us
 [Hint: 153 - 1*1*1 + 5*5*5 + 3*3*3]
 '''
 def armstrong(num) :
+    length_of_num = len(str(num))
+    print (f'The length of num is {length_of_num}.')
     sum = 0
-
-    temp = num
-    while temp > 0:
-       digit = temp % 10
-       sum += digit ** 3
-       temp //= 10
+    arm = num
 
 
-    if num == sum:
-       print(num,"is an Armstrong number")
+    while arm > 0 :
+        digit = arm % 10
+        sum += digit**(length_of_num)
+        arm = arm // 10
+    if sum == num :
+        print("ARMSTRONG")
     else:
-       print(num,"is not an Armstrong number")
+        print("not ARMSTORNG")
 num = int(input("Enter the value:"))
 armstrong(num)
