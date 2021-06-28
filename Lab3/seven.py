633eea2199eb5d3969a4e94366ccd57315e9d04b
Shreeti <ranjit.shreeti1@gmail.com>
2021-06-28 11:32:20 +0545

    upperandlowercase

--- Lab3/seven.py
+++ Lab3/seven.py
@@ -2,5 +2,27 @@
 Write a Python function that accepts a string and calculate the number of upper case letters and lower
 case letters
 '''
-def upperandlower() :
-    up = str(input("Enter the sentence:"))
+def string(name):
+    """
+    This module accepts a string and shows total number uppercase and lowercase used.
+    :param name: string
+    :return: integer
+    """
+    upper = 0
+    lower = 0
+    space = 0
+    for i in name:
+        a = ord(i)  # ord() gives ASCI of characters
+        if a == 32:  # for space
+            space += 1
+        if a >= 65 and a <= 90:  # 65-90 for Upper case
+            upper += 1
+        if a >= 97 and a <= 122:  # 97-122 for Lower case
+            lower += 1
+    total = upper + lower + space
+    print(f"Total lower case character is : {lower}")
+    print(f"Total upper case character is : {upper}")
+    print(f"Total characters in given string is : {total}")
+
+name = "Upper Case Lower Case"
+string(name)
