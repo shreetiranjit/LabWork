ccef4913783524865a868e879398a848b084d990
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-02 14:45:15 +0545

    emptyornot

---
+++ Lab4/list/six.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program to check a list is empty or not.
+'''
+def check(list):
+    if list == [] :
+        print("It is empty")
+    else :
+        print("It isnot empty.")
+check([1,2])
+
