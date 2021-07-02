f6ad572833a60ef2b40f1d9260dc73ac39161b18
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-02 12:37:42 +0545

    starpattern

---
+++ Lab4/four.py
@@ -0,0 +1,21 @@
+'''
+Write a Python program to construct the following pattern,
+ using a nested for loop.
+  *
+  **
+  * * *
+  * * * *
+  * * * * *
+  * * * *
+  * * *
+  * *
+  *
+'''
+for i in range (5) :
+        for j in range (i+1) :
+           print ('*' , end = '')
+        print()
+for i in range (5,0,-1) :
+        for j in range (i) :
+            print ('*' , end = '' )
+        print()
