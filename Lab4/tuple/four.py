3f5cf9ab129e55a3e790c0691f5354676cbff2c7
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-04 10:39:39 +0545

    unpacking_tuple

---
+++ Lab4/tuple/four.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program to unpack a tuple in several variable
+'''
+u_tuple = [1,2,3,4,5]
+print(u_tuple)
+u1 , u2 , u3 , u4 ,u5 = u_tuple
+print(u1)
+print(u2)
+print(u3)
+print(u5 )
+print(u4)
