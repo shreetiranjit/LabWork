7801bf239b46e41c78b7bb0fd0c01101d76aff66
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-04 10:55:26 +0545

    fourth_lastfourth

---
+++ Lab4/tuple/seven.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to get the 4th element and 4th element from last of a tuple.
+'''
+tuple = (1,2,3,4,5,6)
+print(tuple)
+fourth_element = tuple[3]
+print(f'4th element: {fourth_element}')
+fourth_element_from_last = tuple[-4]
+print(f'4th element from last: {fourth_element_from_last}')
