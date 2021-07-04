6da47a0976032920007f4974c16904305e7122c9
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-04 11:26:59 +0545

    intersection

---
+++ Lab4/set/six.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program to create an intersection of sets.
+'''
+a = {1,2,3,4,5}
+b = {3,4,5,6,7,8}
+intersection = a.intersection(b)
+print(intersection)
+print(f' The intersection of {a} and {b} is {intersection}' )
