32edb2a9d3efa773dd72dce4157c3eb2f29d3980
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-06 19:04:35 +0545

    colon

--- Lab4/tuple/eight.py
+++ Lab4/tuple/eight.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 tuple = ("HELLO", 5, [],[])
 print(tuple)
 
-tuple_clone = deepcopy(tuple)
-tuple_clone[2].append(50)
-print(tuple_clone)
-print(tuple)
+tuple_colon = deepcopy(tuple)
+tuple_colon[2].append(50)
+print('deepcopy:' , tuple_colon)
+print('original:' , tuple)
