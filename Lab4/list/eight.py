a0136643d8b6b0f3e45bc77f28999402761f99a4
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-09 09:02:28 +0545

    removing_0_4th_5th

--- Lab4/list/eight.py
+++ Lab4/list/eight.py
@@ -1,5 +1,7 @@
 '''
 Write a Python program to print a specified list after removing the 0th, 4th and 5th elements
 '''
-list = [1,2,3,4,5,6,7]
+list = [1,2,3,4,5,6]
+list = [x for (i,x) in enumerate(list) if i not in (0,4,5)]
+print(list)
 
